65512cb4793dec25a9bda5d6c893e3e05eb4add1
Sarah Faria Rezende <86063513+SarahFeanor@users.noreply.github.com>
2023-06-04 20:10:50 -0300

    Update PROGCOB01.cob

--- PROGCOB01.cob
+++ PROGCOB01.cob
@@ -1,13 +1,13 @@
-           IDENTIFICATION DIVISION.
-           PROGRAM-ID. PROGCOB01.
-      *******************************
-      * AREA DE COMENT¡RIOS - REMAKES
-      * AUTOR = SARAH
-      * OBJETIVO: MOSTRAR A STRING Ol· Mundo
-      * DIA = 09/03/2023
-      *******************************
-           ENVIRONMENT DIVISION.
-           DATA DIVISION.
-           PROCEDURE DIVISION.
-               DISPLAY "Oi mundo".
-               STOP RUN.
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. PROGCOB01.
+      ****************************************
+      ** AUTOR = SARAH F. REZENDE
+      ** OBJETIVO: MOSTRAR A STRING Ol√° Mundo
+      ** DIA = 09/03/2023
+      ****************************************
+        ENVIRONMENT DIVISION.
+        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+        PROCEDURE DIVISION.
+            DISPLAY "Oi mundo".
+            STOP RUN.
